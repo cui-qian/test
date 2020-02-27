11f1490f8be0d2bd0391b60b1971fed5ef9af9f9
cuiqian <780540998@qq.com>
2020-02-27 14:56:53 +0800

    项目第一次提交

--- js/login.js
+++ js/login.js
@@ -1,4 +1,6 @@
 $(function () {
+    // 只要导入了layui的脚本,就可以使用layui.form
+    var form = layui.form;
     // 点击了注册的链接
     $('#link-reg').on('click', function () {
         $('.login-box').hide();
@@ -9,4 +11,21 @@ $(function () {
         $('.login-box').show();
         $('.reg-box').hide();
     });
+
+
+    // 自定义校验规则
+    form.verify({
+        // 键:值
+        psd: [/^[\S]{6,12}$/], //'密码必须6到12位，且不能出现空格'
+        samePsd: function (value) {
+            // 1.通过形参,获取到确认密码框中的值
+            // 2.通过jquery获取到密码框中的值
+            var psd = $('.reg-box [name=password]').val();
+            // 3.进行if判断
+            if (value !== psd) {
+                return '两次的密码不一致!'
+            }
+
+        }
+    })
 })
